b040f581272735621ead120074199fb297aea987
Matthew Wilson <matthewwilson@Matthews-MacBook-Pro.local>
2023-09-01 17:22:50 +0100

    add meals app

---
+++ 03-starting-project-meals/src/components/Meals/MealsSummary.js
@@ -0,0 +1,13 @@
+import classes from "./MealsSummary.module.css"
+
+const MealsSummary = () => {
+    return (
+        <section className={classes.summary}>
+            <h2>Delicious Food</h2>
+            <p>meals</p>
+            <p>more meals</p>
+        </section>
+    )
+}
+
+export default MealsSummary;
