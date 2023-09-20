976ae6a73eb31663216440fc469f328eea870685
Matthew Wilson <matthewwilson@192.168.1.102>
2023-09-20 16:24:56 +0100

    complete reducer and functionality

--- 03-starting-project-meals/src/components/Layout/HeaderCartButton.js
+++ 03-starting-project-meals/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,43 @@
+import { useContext, useEffect, useState } from "react";
+
 import classes from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const [btnHighlighted, setBtnHighlighted] = useState(false);
+  const cartCtx = useContext(CartContext);
+
+  const numberOfItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0)
+
+  const { items } = cartCtx;
+
+  const btnClasses = `${classes.button} ${btnHighlighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0 ) {
+      return;
+    }
+    setBtnHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items]);
+
   return (
-    <button className={classes.button}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>2</span>
+      <span className={classes.badge}>{numberOfItems}</span>
     </button>
   );
 };
